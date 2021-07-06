c9f0ca9e30484636f341f43e8c7fc0638a0472ff
MKS-01 <manish10mks@gmail.com>
2021-07-06 10:22:14 +0530

    signIn screen add

--- client/TechNewsExpo/src/scenes/SignIn/index.js
+++ client/TechNewsExpo/src/scenes/SignIn/index.js
@@ -1,12 +1,17 @@
 import React, { useState, useContext } from "react";
-import { View, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import PressableBtn from "_components/common/Buttons/PressableBtn";
 import TextInput from "_components/common/TextInput";
 import { AuthContext } from "_hooks/AuthHook";
-import { RootScrollView, RootSafeArea } from "_styles/RootView";
+import { FONT_FAMILY_SEMIBOLD } from "_styles/typography";
+import { RootSafeArea } from "_styles/RootView";
 
 const SignInScreen = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigation = useNavigation();
 
   const { signIn } = useContext(AuthContext);
 
@@ -34,8 +39,9 @@ const SignInScreen = () => {
           onChangeText={setPassword}
         />
 
-        <Button
-          title="Sign in"
+        <View style={{ marginTop: 10 }} />
+        <PressableBtn
+          title="Sign In"
           onPress={() =>
             signIn({
               username,
@@ -43,9 +49,32 @@ const SignInScreen = () => {
             })
           }
         />
+        <View style={styles.textContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+            <Text style={styles.text}>Sign Up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Forgot")}>
+            <Text style={styles.text}>Forgot Password?</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </RootSafeArea>
   );
 };
+
+const styles = StyleSheet.create({
+  textContainer: {
+    flexDirection: "row",
+    marginTop: 15,
+    justifyContent: "space-around",
+    width: `60%`,
+    alignSelf: "center",
+  },
+  text: {
+    fontFamily: FONT_FAMILY_SEMIBOLD,
+    color: "#cacaca",
+    textDecorationLine: "underline",
+  },
+});
 
 export default SignInScreen;
